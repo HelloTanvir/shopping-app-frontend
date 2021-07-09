49d08f8a0f99134378a58184928bcf6410dfb480
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-07-09 10:26:14 +0600

    useCallback hook added in DeviceContext to optimise performance

--- src/contexts/DeviceContext.jsx
+++ src/contexts/DeviceContext.jsx
@@ -1,30 +1,24 @@
-import { createContext, useState } from 'react';
+import { createContext, useCallback, useEffect, useState } from 'react';
 
 export const DeviceContext = createContext();
 
 const DeviceContextProvider = ({ children }) => {
-    const [device, setDevice] = useState(
-        window.innerWidth > 1015
-            ? 'desktop'
-            : window.innerWidth >= 789 && window.innerWidth <= 1015
-            ? 'tablet'
-            : window.innerWidth >= 679 && window.innerWidth < 789
-            ? 'mobile'
-            : window.innerWidth < 679
-            ? 'sm-mobile'
-            : ''
-    );
+    const [device, setDevice] = useState('');
 
-    const handleDeviceSet = () => {
+    const handleDeviceSet = useCallback(() => {
         if (device !== 'desktop' && window.innerWidth > 1015) setDevice('desktop');
         else if (device !== 'tablet' && window.innerWidth >= 789 && window.innerWidth <= 1015)
             setDevice('tablet');
         else if (device !== 'mobile' && window.innerWidth >= 679 && window.innerWidth < 789)
             setDevice('mobile');
         else if (device !== 'sm-mobile' && window.innerWidth < 679) setDevice('sm-mobile');
-    };
+    }, [device]);
 
     window.addEventListener('resize', handleDeviceSet);
+
+    useEffect(() => {
+        handleDeviceSet();
+    }, [handleDeviceSet]);
 
     return <DeviceContext.Provider value={{ device }}>{children}</DeviceContext.Provider>;
 };
